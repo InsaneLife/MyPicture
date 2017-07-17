7f4816df5dea21d97ed6bbc5ab4ee1c994ace11e
AaronChou <993001803@qq.com>
2017-07-17 20:52:51 +0800

    Update ensemble_blending.py

--- ensemble_blending.py
+++ ensemble_blending.py
@@ -26,27 +26,28 @@ X, X_predict, y, y_predict = train_test_split(data, target, test_size=0.33, rand
 n_folds = 5
 skf = list(StratifiedKFold(y, n_folds))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=2017)
-dataset_blend_train = np.zeros((X_test.shape[0], len(clfs)))
-dataset_blend_test = np.zeros((X_predict.shape[0], len(clfs)))
+'''切分训练数据集为d1,d2两部分'''
+X_d1, X_d2, y_d1, y_d2 = train_test_split(X, y, test_size=0.5, random_state=2017)
+dataset_d1 = np.zeros((X_d2.shape[0], len(clfs)))
+dataset_d2 = np.zeros((X_predict.shape[0], len(clfs)))
 
 for j, clf in enumerate(clfs):
     '''依次训练各个单模型'''
     # print(j, clf)
     '''使用第1个部分作为预测，第2部分来训练模型，获得其预测的输出作为第2部分的新特征。'''
     # X_train, y_train, X_test, y_test = X[train], y[train], X[test], y[test]
-    clf.fit(X_train, y_train)
-    y_submission = clf.predict_proba(X_test)[:, 1]
-    dataset_blend_train[:, j] = y_submission
+    clf.fit(X_d1, y_d1)
+    y_submission = clf.predict_proba(X_d2)[:, 1]
+    dataset_d1[:, j] = y_submission
     '''对于测试集，直接用这k个模型的预测值作为新的特征。'''
-    dataset_blend_test[:, j] = clf.predict_proba(X_predict)[:, 1]
-    print("val auc Score: %f" % roc_auc_score(y_predict, dataset_blend_test[:, j]))
+    dataset_d2[:, j] = clf.predict_proba(X_predict)[:, 1]
+    print("val auc Score: %f" % roc_auc_score(y_predict, dataset_d2[:, j]))
 
 '''融合使用的模型'''
 # clf = LogisticRegression()
 clf = GradientBoostingClassifier(learning_rate=0.02, subsample=0.5, max_depth=6, n_estimators=30)
-clf.fit(dataset_blend_train, y_test)
-y_submission = clf.predict_proba(dataset_blend_test)[:, 1]
+clf.fit(dataset_d1, y_d2)
+y_submission = clf.predict_proba(dataset_d2)[:, 1]
 
 print("Linear stretch of predictions to [0,1]")
 y_submission = (y_submission - y_submission.min()) / (y_submission.max() - y_submission.min())
